a4ff333ecf99c7e1f2abdc4eae4434b067c16eee
Michele Mazzucco <hello@michelemazzucco.it>
2016-08-30 11:47:46 +0200

    add new plugin for size, super simple

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require('gulp')
     , postcss = require('gulp-postcss');
 
 // PostCSS plugins
+var size = require('postcss-size');
 var cssnext = require('postcss-cssnext');
 var cssnano = require('cssnano');
 
 gulp.task('css', function () {
 
   var processors = [
+    size(),
     cssnext({browsers: ['last 10 versions']}),
     cssnano({core: false, autoprefixer: false, safe: true})
   ];
